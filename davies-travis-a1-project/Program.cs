441f30450aa39c6956fc38462d78c9a93774bf48
Travis Davies <000967345@mohawkcollege.ca>
2025-01-19 16:36:34 -0500

    Declare Valuables, Add Player Info Quiz

--- davies-travis-a1-project/Program.cs
+++ davies-travis-a1-project/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string travellerOneFirst = "Jeff";
+        string travellerTwoFirst = "Sue";
+        string travellerThreeFirst = "Joshua";
+        string travellerOneLast = "Johnson";
+        string travellerTwoLast = "Williams";
+        string travellerThreeLast = "Joshin";
+        int allow = 1;
+        int deny = 2;
+        int detain = 3;
+        bool travellerOneDetain = false;
+        bool travellerTwoDetain = true;
+        bool travellerThreeDetain = false;
+        bool travellerOneAllow = true;
+        bool travellerTwoAllow = false;
+        bool travellerThreeAllow = false;
+
+        Console.WriteLine($"Welcome to your new job as a border guard agent employee.\nIt appears your information has not been filled into our system.\nPlease kindly insert your first name.");
+        string playerFirst = Console.ReadLine();
+        Console.WriteLine("And your last name.");
+        string playerLast = Console.ReadLine();
+        Console.WriteLine("Finally, please enter an employee ID number. It can be any number you desire.");
+        int playerId = int.Parse(Console.ReadLine());  
+        string playerName = $"{playerFirst} {playerLast}";
+        Console.WriteLine($"Thank you, {playerName} with employee ID number {playerId}.");
+
+    }
+}
